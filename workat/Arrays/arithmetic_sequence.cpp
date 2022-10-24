5c2dbff25f56609e4e06b57d3a20f7345c747563
Omar ElGarhy <omar.k.elgarhy@gmail.com>
2022-10-24 07:48:33 +0200

    workat/arrays/arithmetic-sequence

---
+++ workat/Arrays/arithmetic_sequence.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool isArithmeticSequence(vector<int> &arr)
+{
+  sort(arr.begin(), arr.end());
+  int diff = arr[1] - arr[0];
+
+  for (int i = 1; i < arr.size() - 1; i++)
+  {
+    if (arr[i + 1] - arr[i] != diff)
+    {
+      return false;
+    }
+  }
+  return true;
+}
